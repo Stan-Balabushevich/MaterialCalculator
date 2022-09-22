3a8c950eef1c0b0d4f7c47f709d46851d1382a49
Stan-Balabushevich <stasntnew@gmail.com>
2022-09-22 17:54:07 +0200

    Build variables types

---
+++ app/src/debug/java/com/slava/test/materialcalculator/Constants.kt
@@ -0,0 +1,6 @@
+package com.slava.test.materialcalculator
+
+object Constants {
+
+    const val BASE_URL = "debug url"
+}
--- app/src/main/java/com/slava/test/materialcalculator/MainActivity.kt
+++ app/src/main/java/com/slava/test/materialcalculator/MainActivity.kt
@@ -9,6 +9,7 @@ import com.slava.test.materialcalculator.ui.theme.MaterialCalculatorTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             MaterialCalculatorTheme {
                 CalculatorScreen()
